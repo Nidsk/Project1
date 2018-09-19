698b253859ca77a39f6b87595117d28377fd1ce6
khand <khand@LAPTOP-Q6H48UGC>
2018-09-19 15:38:13 +0530

    commit 1

---
+++ ScreenShots/src/captureScreenshot/FacebookScreenShot.java
@@ -0,0 +1,32 @@
+package captureScreenshot;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FacebookScreenShot {
+		
+		@Test
+		public void captureScrnshot() throws Exception {
+			System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.facebook.com/");
+			driver.findElement(By.xpath("//input[@type='email']")).sendKeys("Nidhi");
+			TakesScreenshot ts=(TakesScreenshot) driver;
+			File source = ts.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(source, new File("./Screenshots/facebook.png"));
+			System.out.println("Screenshot taken");
+			driver.quit();
+			
+		}
+	
+
+}
